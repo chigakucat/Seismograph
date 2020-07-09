51ff34aecbee362a61ed54b45f961017d05adef2
chigakucat <35723931+chigakucat@users.noreply.github.com>
2020-07-09 19:04:03 +0900

    Add files via upload

---
+++ graphdraw.py
@@ -0,0 +1,13 @@
+# -*- Coding: utf-8 -*-
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+data = pd.read_csv('20200506_senkoudata.csv',encoding = 'UTF8', names = ['time','x', 'y', 'z', 'frame','f','g','h','i','j','k','l'])
+#plt.plot(data['time'], data['x'])
+#plt.plot(data['time'], data['y'])
+plt.plot(data['time'], data['z'], color ='black', linewidth =0.9) 
+
+plt.savefig('graph.png')
+plt.show()
