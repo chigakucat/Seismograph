413bb7910d1dc3a213d6a371042d68073c5faca3
chigakucat <35723931+chigakucat@users.noreply.github.com>
2021-12-12 21:55:49 +0900

    Update graphdraw_py3.py

--- graphdraw_py3.py
+++ graphdraw_py3.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 
 
 data = pd.read_csv('test.csv', encoding = 'UTF8', names = ['time', 'date', 'x', 'y', 'z', 'frame', 'f'])
-#plt.plot(data['time'], data['x'], color = 'black', linewidth = 0.9)
-plt.plot(data['time'], data['y'], color = 'black', linewidth = 0.9)
+plt.plot(data['time'], data['x'], color = 'black', linewidth = 0.9)
+#plt.plot(data['time'], data['y'], color = 'black', linewidth = 0.9)
 #plt.plot(data['time'], data['z'], color ='black', linewidth =0.9) 
 
 plt.show()
