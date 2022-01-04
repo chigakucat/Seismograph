5eee52068194bf87dbc4d38312ebcb14f5bdd352
chigakucat <35723931+chigakucat@users.noreply.github.com>
2022-01-04 18:41:41 +0900

    data_dump.sh 修正

--- data_dump.sh
+++ data_dump.sh
@@ -2,9 +2,8 @@
 
 cd `dirname $0`
 cd rpi-seismometer
-pkill -f ~/Seismograph/kasokudo_py3.py
+sudo pkill -f ~/Seismograph/kasokudo_py3.py
 sleep 1
-mv shind.csv shind_data_$(date +%Y%m%d).csv
-touch shind.csv
-sleep 1
-python3 ~/Seismograph/kasokudo_py3.py >> kasokudo.csv
+mv kasokudo.csv kasokudo_data_$(date +%Y%m%d).csv
+touch kasokudo.csv
+python3 /home/pi/Seismograph/kasokudo_py3.py >> /home/pi/Seismograph/kasokudo.csv
