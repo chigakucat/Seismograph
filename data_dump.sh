d085b738bdd6b3d0ab211dde40e014e235c78629
chigakucat <35723931+chigakucat@users.noreply.github.com>
2022-04-18 20:30:21 +0900

    Add files via upload

--- data_dump.sh
+++ data_dump.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd `dirname $0`
+cd
 cd Seismograph
 sudo pkill -f ~/Seismograph/kasokudo_py3.py
 sleep 1
 mv kasokudo.csv kasokudo_data_$(date +%Y%m%d).csv
 touch kasokudo.csv
-python3 /home/pi/Seismograph/kasokudo_py3.py >> /home/pi/Seismograph/kasokudo.csv
+python3 kasokudo_py3.py >> kasokudo.csv
--- seis_setup.sh
+++ seis_setup.sh
@@ -11,34 +11,22 @@ sudo apt -y autoclean
 sudo raspi-config nonint do_vnc 0
 sudo raspi-config nonint do_ssh 0
 sudo raspi-config nonint do_spi 0
+sudo raspi-config nonint do_boot_behaviour B3
 
-python3 --version &> /dev/null
-if [ $? -ne 0 ] ; then
   echo Install Python3
-  sudo apt install python3
-else
-  echo Python3 is already installed.
-fi
+  sudo apt -y install python3
 
-vim --version &> /dev/null
-if [ $? -ne 0 ] ; then
   echo Install vim
-  sudo apt install vim
-else
-  echo vim is already installed.
-fi
+  sudo apt -y install vim
 
 python3 -m pip install spidev
 
-cd Seismograph &> /dev/null
-if [ $? -ne 0 ] ; then
+cd Seismograph
   crontab seis_cron.txt
   rm -r old
+
   echo All setup is complete.
   echo Automatically reboots after 15 seconds.
-  sleep 10
-  echo reboot
+  sleep 15
+  echo reboot...
   sudo reboot
-else
-  echo error.
-fi
